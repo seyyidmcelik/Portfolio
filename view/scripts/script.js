e08c7f29ce8d995cc7140af6252f5b23a410408c
seyyidmcelik <seyyidmcelik@gmail.com>
2021-09-17 17:08:07 +0300

    Menu bugs fixed

--- view/scripts/script.js
+++ view/scripts/script.js
@@ -1,7 +1,15 @@
-let burger = document.querySelector('.burger')
-let mobileMenu = document.querySelector('.mobile-menu')
+let burger = document.querySelector(".burger");
+let mobileMenu = document.querySelector(".mobile-menu");
+let links = document.querySelectorAll(".mobile-menu ul li a");
 
-burger.addEventListener('click',()=>{
-    burger.classList.toggle('active-burger')
-    mobileMenu.classList.toggle('active-mobile-menu')
-})
+burger.addEventListener("click", () => {
+  burger.classList.toggle("active-burger");
+  mobileMenu.classList.toggle("active-mobile-menu");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    burger.classList.toggle("active-burger");
+    mobileMenu.classList.toggle("active-mobile-menu");
+  });
+});
